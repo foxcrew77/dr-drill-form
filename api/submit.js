23eb62bf262f6d83006c9126ea22d7461726a592
Hanif <hanif@sabah-net.com>
2025-06-02 00:25:31 +0800

    Fix Vercel serverless function using option 3

--- api/submit.js
+++ api/submit.js
@@ -1,9 +1,6 @@
-const fetch = require('node-fetch');
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      // Forward the request body to your Apps Script Web App
       const response = await fetch('https://script.google.com/macros/s/AKfycbzdUboTdeHvwCNJp29e2khdwSgPxbHymYtQHeh4Nq0v87_eTX8EYhZ4x0J23GO8K9l6/exec', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -17,4 +14,4 @@ module.exports = async (req, res) => {
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
-};
+}
