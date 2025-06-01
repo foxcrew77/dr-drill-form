9a63f310825def800e9c542d4409f39d7e6ebcd7
Hanif <hanif@sabah-net.com>
2025-06-02 02:55:01 +0800

    change deployment script

--- api/submit.js
+++ api/submit.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       // Use your working Apps Script Web App URL
-      const response = await fetch('https://script.google.com/macros/s/AKfycbzgAdAf-CIDJlJlMoeHUcMVY9XyDEYYXKHDkhDQIoNWpXJ-EzAlHt8s8HuduS2CugxbHg/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbxDA6EairvLNbDnm8dl4bFznGIaYsgHCYG-3s6p2TuRGSC3ONCoqHlg4O_aZOevtfeSow/exec', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body)
